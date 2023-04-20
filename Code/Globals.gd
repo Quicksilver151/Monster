2aa6f139368408a240a1c1a688c6fc980f6831b8
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-04-20 22:52:36 +0500

    added more environment textures, fullscreen code

--- Code/Globals.gd
+++ Code/Globals.gd
@@ -2,3 +2,17 @@ extends Node
 
 var health = 10
 var energy = 5
+
+
+
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("f_fullscreen"):
+		toggle_fullscreen()
+
+
+func toggle_fullscreen():
+	match DisplayServer.window_get_mode():
+		3 : DisplayServer.window_set_mode(0)
+		0 : DisplayServer.window_set_mode(3)
+
